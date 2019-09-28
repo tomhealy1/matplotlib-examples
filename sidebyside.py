a4e1df1dbdfcbdaeb395f556788f8d1c244d9baa
tomhealy1 <alfaone156@gmail.com>
2019-09-28 13:48:49 +0100

    Added side by side

---
+++ sidebyside.py
@@ -0,0 +1,15 @@
+#2 plot side by side
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+plt.subplot(1, 2, 1)
+x = np.random.normal(0.0, 1.0, 10000)
+
+#More bins = more granularity
+plt.hist(x,bins=20)
+
+plt.subplot(1, 2, 2)
+x = np.random.normal(-3.0, 3.0, 10000)
+plt.hist(x)
+
+plt.show()
